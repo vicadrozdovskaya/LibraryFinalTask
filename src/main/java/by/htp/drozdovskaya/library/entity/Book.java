12bdc6531b964208d42bb58196a0ede2d59b7a59
Victoria <vica.drozdovskaya@gmail.com>
2018-07-30 11:41:20 +0300

    add Entities in project

---
+++ src/main/java/by/htp/drozdovskaya/library/entity/Book.java
@@ -0,0 +1,102 @@
+package by.htp.drozdovskaya.library.entity;
+
+public class Book {
+	
+	private int idBook;
+	private String title;
+	private int quantity;
+	private Author author;
+	
+	public Book() {
+	
+	}
+
+	public Book(int idBook, String title, int quantity, Author author) {
+		this.idBook = idBook;
+		this.title = title;
+		this.quantity = quantity;
+		this.author = author;
+	}
+
+	
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public Author getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(Author author) {
+		this.author = author;
+	}
+
+	public int getIdBook() {
+		return idBook;
+	}
+
+	public void setIdBook(int idBook) {
+		this.idBook = idBook;
+	}
+
+	public int getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((author == null) ? 0 : author.hashCode());
+		result = prime * result + idBook;
+		result = prime * result + quantity;
+		result = prime * result + ((title == null) ? 0 : title.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Book other = (Book) obj;
+		if (author == null) {
+			if (other.author != null)
+				return false;
+		} else if (!author.equals(other.author))
+			return false;
+		if (idBook != other.idBook)
+			return false;
+		if (quantity != other.quantity)
+			return false;
+		if (title == null) {
+			if (other.title != null)
+				return false;
+		} else if (!title.equals(other.title))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "\nBook: id=" + idBook + ", title=" + title + ", quantity=" + quantity + ", " + author;
+	}
+	
+	
+	
+	
+	
+	
+
+}
